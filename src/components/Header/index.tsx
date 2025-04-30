c74bf90e64a214426acc1d2b3bdadafeee09c00b
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-04-30 22:52:38 +0900

    fix unnecessary implementation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,3 @@
-import { Outlet } from "react-router";
 import "./style.css";
 
 export const Header = () => {
@@ -7,7 +6,6 @@ export const Header = () => {
       <header className="header">
         <h1 className="title">REACT UPDATE</h1>
       </header>
-      <Outlet />
     </>
   );
 };
