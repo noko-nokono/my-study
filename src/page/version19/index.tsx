c73caa10d00529ce589dc0e25216b472fcfdadd9
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-05-01 23:40:07 +0900

    add page

--- src/page/version19/index.tsx
+++ src/page/version19/index.tsx
@@ -1,5 +1,27 @@
+import './style.css';
+
 export const Version19 = () => {
   return (
-    <div>React Version 19</div>
+    <div className='version19_root'>
+      <div className='version19_container'>
+        <h1>React 19</h1>
+        <a
+          href="https://react.dev/blog/2024/12/05/react-19"
+          target='_blank'
+        >
+          https://react.dev/blog/2024/12/05/react-19
+        </a>
+      </div>
+
+      <div>
+        <h2>hogehoge</h2>
+        <p>ああああ</p>
+      </div>
+
+      <div>
+        <h2>fugafuga</h2>
+        <p>いいいい</p>
+      </div>
+    </div>
   )
 };
