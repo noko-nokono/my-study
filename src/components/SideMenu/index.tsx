4a3e5b6cfd23fa5b66946cac39d0764ab1682fed
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-05-01 23:24:31 +0900

    add routing

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,9 +1,20 @@
 import './style.css';
 
 export const SideMenu = () => {
+  const menuItems = [
+    { name: 'Home', path: '/' },
+    { name: 'version19', path: '/version19' },
+  ];
+
   return (
     <aside className="side_menu">
-      <h2>Side Menu</h2>
+      <ul className='menu_list'>
+        {menuItems.map((item) => (
+          <li>
+            <a href={ item.path }>{ item.name }</a>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 };
