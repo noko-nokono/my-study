db9490a440c8462f9c4d51a5542d179b1e05f4c4
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-05-11 11:57:52 +0900

    refactor common api

--- src/page/version19/component/useActionState.tsx
+++ src/page/version19/component/useActionState.tsx
@@ -1,27 +1,7 @@
 import { useActionState } from 'react';
-
-type FormState = { id: string; name: string };
+import { updateName } from '@/apis/update';
 
 export const ActionState = () => {
-  const updateName = async (name: FormState | null): Promise<FormState> => {
-    // 検証のため1秒間意図的に遅延させる
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    const option = {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ mock: { name } }),
-    };
-
-    const response = await fetch(`https://noko_nokono.com/`, option);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  };
-
   const [state, submitAction, isPending] = useActionState(updateName, null);
 
   return (
--- src/page/version19/component/useTransition.tsx
+++ src/page/version19/component/useTransition.tsx
@@ -1,32 +1,14 @@
 import { useState, useTransition } from "react";
+import { updateName } from '@/apis/update';
 
 export const Transition = () => {
   const [name, setName] = useState("");
   const [error, setError] = useState(null);
   const [isPending, startTransition] = useTransition();
 
-  const updateName = async (name: string): Promise<{ id: string; name: string }> => {
-    // 検証のため1秒間意図的に遅延させる
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    const option = {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ mock: { name } }),
-    };
-
-    const response = await fetch(`https://noko_nokono.com/`, option);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  };
-
   const handleSubmit = () => {
     startTransition(async () => {
-      const res = await updateName(name);
+      const res = await updateName({id: "", name});
       if (!res.id) {
         setError(error);
         return;
