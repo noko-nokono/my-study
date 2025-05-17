8689fba3979432fd8d73a2859a2c5c8b50f50b67
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-05-17 12:34:33 +0900

    fix useFromState

--- src/page/version19/component/useFormStatus.tsx
+++ src/page/version19/component/useFormStatus.tsx
@@ -1,12 +1,17 @@
-import { useActionState } from 'react';
+import { useState, useActionState } from 'react';
 import { updateName } from '@/apis/update';
 import { FormStatusItem } from './useFormStatusItem';
 
 export const FormStatus = () => {
-  const [state, submitAction] = useActionState(updateName, null);
+  const [name, setName] = useState("");
+  const [state, submitAction] = useActionState(
+    () => updateName({ id: "", name }),
+    null
+  );
+
   return (
     <form action={submitAction}>
-      <input type="text" name="name" />
+      <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
       <FormStatusItem />
       {state && <p>{ state.name }</p>}
     </form>
