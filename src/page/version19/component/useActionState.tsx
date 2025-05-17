1a4643ea88b03cd7cee425a6f4aac31f9fe7f287
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-05-17 12:32:53 +0900

    fix useActionState

--- src/page/version19/component/useActionState.tsx
+++ src/page/version19/component/useActionState.tsx
@@ -1,12 +1,16 @@
-import { useActionState } from 'react';
+import { useState, useActionState } from 'react';
 import { updateName } from '@/apis/update';
 
 export const ActionState = () => {
-  const [state, submitAction, isPending] = useActionState(updateName, null);
+  const [name, setName] = useState("");
+  const [state, submitAction, isPending] = useActionState(
+    () => updateName({ id: "", name }),
+    { id: "", name }
+  );
 
   return (
     <form action={submitAction}>
-      <input type="text" name="name" />
+      <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
       <button type="submit" disabled={isPending}>Update</button>
       {state && <p>{ state.name }</p>}
     </form>
