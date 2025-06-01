624d644c355f143e289662bd74695232a83fda10
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-06-01 23:25:02 +0900

    add server component

--- src/page/server-component/index.tsx
+++ src/page/server-component/index.tsx
@@ -2,6 +2,18 @@ import './style.css';
 
 export const ServerComponent = () => {
   return (
-    <div>ServerComponent</div>
+    <div className='root'>
+      <h2 className='title'>ServerComponent</h2>
+      <p> This means libraries that ship with Server Components can now target React 19 as a peer dependency with a react-server export condition for use in frameworks that support the Full-stack React Architecture.</p>
+      <p>
+        React version 19 で追加されたサーバーコンポーネントは、フレームワークで使用されることを前提として機能の追加になっている。
+        <br />
+        React Server Component では、Webサーバーを使用しないものと、使用するものの2つの種類が用意されており、一般的にサーバーコンポーネントとして言われているのはWebサーバーを使用する方。
+      </p>
+      <p>使用しないものは、ビルド時に</p>
+      <p>
+        <a href="https://ja.react.dev/reference/rsc/server-components" target='_blank'>https://ja.react.dev/reference/rsc/server-components</a>
+      </p>
+    </div>
   )
 };
