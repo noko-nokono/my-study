6e3f39cc71e627dcc483cb5ff929f740c7180967
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-06-07 23:08:16 +0900

    change title

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <>
       <header className="header">
-        <h1 className="title">REACT UPDATE</h1>
+        <h1 className="title">My Study</h1>
       </header>
     </>
   );
