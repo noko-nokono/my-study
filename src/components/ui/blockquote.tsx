1c10ab1a80d31da7ed2a76ec5ba702f5537d9d1d
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-06-22 23:30:59 +0900

    add blockquote component

---
+++ src/components/ui/blockquote.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  children: React.ReactNode;
+}
+
+export const TypographyBlockquote = ({ children }: Props) => {
+  return (
+    <blockquote className="mt-6 border-l-2 pl-6 italic">
+      { children }
+    </blockquote>
+  )
+}
