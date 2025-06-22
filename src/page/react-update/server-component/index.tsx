0fb42df5e5f442e048672dd104ea559ca2ec58a5
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-06-23 08:51:33 +0900

    refactor server component page

--- src/page/react-update/server-component/index.tsx
+++ src/page/react-update/server-component/index.tsx
@@ -1,35 +1,10 @@
-import { Blockquote, H3 } from '@/components/ui';
+// import { Blockquote, H3 } from '@/components/ui';
 import Markdown from '@/components/common/Markdown';
-
-const page =
-`# ServerComponent
-
-This means libraries that ship with Server Components can now target React 19 as a peer dependency with a react-server export condition for use in frameworks that support the Full-stack React Architecture.  
-
-React version 19 で追加されたサーバーコンポーネントは、フレームワークで使用されることを前提として機能の追加になっている。
-React Server Component では、Webサーバーを使用しないものと、使用するものの2つの種類が用意されており、一般的にサーバーコンポーネントとして言われているのはWebサーバーを使用する方。
-使用しないものは、ビルド時にビルドされる。
-
-## Hello World
-こんにちは！
-`;
+import { page } from './page';
 
 export const ServerComponent = () => {
   return (
     <div className='flex flex-col gap-4'>
-      <H3>ServerComponent</H3>
-      <Blockquote>
-        This means libraries that ship with Server Components can now target React 19 as a peer dependency with a react-server export condition for use in frameworks that support the Full-stack React Architecture.  
-      </Blockquote>
-      <p>
-        React version 19 で追加されたサーバーコンポーネントは、フレームワークで使用されることを前提として機能の追加になっている。
-        <br />
-        React Server Component では、Webサーバーを使用しないものと、使用するものの2つの種類が用意されており、一般的にサーバーコンポーネントとして言われているのはWebサーバーを使用する方。
-      </p>
-      <p>使用しないものは、ビルド時に</p>
-      <p>
-        <a href="https://ja.react.dev/reference/rsc/server-components" target='_blank'>https://ja.react.dev/reference/rsc/server-components</a>
-      </p>
       <Markdown page={page} />
     </div>
   )
---
+++ src/page/react-update/server-component/page.ts
@@ -0,0 +1,14 @@
+export const page =
+`
+# ServerComponent
+
+> This means libraries that ship with Server Components can now target React 19 as a peer dependency with a react-server export condition for use in frameworks that support the Full-stack React Architecture.
+
+React version 19 で追加されたサーバーコンポーネントは、フレームワークで使用されることを前提として機能の追加になっている。
+React Server Component では、Webサーバーを使用しないものと、使用するものの2つの種類が用意されており、一般的にサーバーコンポーネントとして言われているのはWebサーバーを使用する方。
+
+使用しないものは、ビルド時に...
+
+以下公式ドキュメント
+https://ja.react.dev/reference/rsc/server-components
+`
