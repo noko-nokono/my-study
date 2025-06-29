21a477d4712421055c2aa05636ac2ad83bdb88e0
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-06-29 23:02:31 +0900

    add react version page

--- src/page/react-update/version19/index.tsx
+++ src/page/react-update/version19/index.tsx
@@ -3,64 +3,44 @@ import { ActionState } from './component/useActionState';
 import { FormStatus } from './component/useFormStatus';
 import { Optimistic } from './component/useOptimistic';
 import { UseParent } from './component/useParent';
+import Markdown from '@/components/common/Markdown';
+import {
+  page,
+  useTransitionPage,
+  useActionStatePage,
+  useFormStatusPage,
+  useOptimisticPage,
+  useParentPage
+} from './page';
 
 export const Version19 = () => {
   return (
     <div className='flex flex-col gap-8 h-full'>
-      <div className='flex flex-col gap-2'>
-        <h1>React 19</h1>
-        <a
-          href="https://react.dev/blog/2024/12/05/react-19"
-          target='_blank'
-        >
-          https://react.dev/blog/2024/12/05/react-19
-        </a>
-      </div>
+      <Markdown page={page} />
 
       <div>
-        <p>useTransition</p>
+        <Markdown page={useTransitionPage} />
         <Transition />
-        <p>[メモ]</p>
-        <p>useTransitionを使用することで、非同期な更新処理に対してのpendingの状態を今までより簡単に管理できるようになる。</p>
-        <p>[document]</p>
-        <a href='https://ja.react.dev/reference/react/useTransition' target='_blank'>https://ja.react.dev/reference/react/useTransition</a>
       </div>
 
       <div>
-        <p>useActionState</p>
+        <Markdown page={useActionStatePage} />
         <ActionState />
-        <p>[メモ]</p>
-        <p>useActionStateを使用することで、フォームの値の管理を簡略化してくれる。</p>
-        <p>useTransitionを包括する形で説明されていたので、基本はこちらを使うことになりそう？</p>
-        <p>[document]</p>
-        <a href='https://ja.react.dev/reference/react/useActionState' target='_blank'>https://ja.react.dev/reference/react/useActionState</a>
       </div>
 
       <div>
-        <p>useFormStatus</p>
+        <Markdown page={useFormStatusPage} />
         <FormStatus />
-        <p>[メモ]</p>
-        <p>子要素にpropsとしてフォームのデータを渡すのではなく、カスタムフックといて機能を提供してくれている。</p>
-        <p>[document]</p>
-        <a href='https://ja.react.dev/reference/react-dom/hooks/useFormStatus' target='_blank'>https://ja.react.dev/reference/react-dom/hooks/useFormStatus</a>
       </div>
 
       <div>
-        <p>useOptimistic</p>
+        <Markdown page={useOptimisticPage} />
         <Optimistic />
-        <p>[メモ]</p>
-        <p>非同期の更新を行う際に楽観的な評価を行い、APIのレスポンスの前に入力された値を表示する。</p>
-        <p>[document]</p>
-        <a href='https://ja.react.dev/reference/react/useOptimistic' target='_blank'>https://ja.react.dev/reference/react/useOptimistic</a>
       </div>
 
       <div>
-        <p>use</p>
+        <Markdown page={useParentPage} />
         <UseParent />
-        <p>[メモ]</p>
-        <p>非同期のデータ取得を行なってくれる（fetch関数の代わりみたいなイメージ）。Suspenseを使用することで、データ取得中はローディング画面を表示することができる。</p>
-        <p>[document]</p>
-        <a href='https://ja.react.dev/reference/react/use' target='_blank'>https://ja.react.dev/reference/react/use</a>
       </div>
     </div>
   )
---
+++ src/page/react-update/version19/page.ts
@@ -0,0 +1,41 @@
+// 仮の実装
+export const page =
+`# React 19
+
+公式ドキュメント: https://react.dev/blog/2024/12/05/react-19`;
+
+export const useTransitionPage =
+`### useTransition
+https://ja.react.dev/reference/react/useTransition
+
+useTransitionを使用することで、非同期な更新処理に対してのpendingの状態を今までより簡単に管理できるようになる。
+`;
+
+export const useActionStatePage =
+`### useActionState
+https://ja.react.dev/reference/react/useActionState
+
+useActionStateを使用することで、フォームの値の管理を簡略化してくれる。
+useTransitionを包括する形で説明されていたので、基本はこちらを使うことになりそう？
+`;
+
+export const useFormStatusPage =
+`### useFormStatus
+https://ja.react.dev/reference/react-dom/hooks/useFormStatus
+
+子要素にpropsとしてフォームのデータを渡すのではなく、カスタムフックといて機能を提供してくれている。
+`;
+
+export const useOptimisticPage =
+`### useOptimistic
+https://ja.react.dev/reference/react/useOptimistic
+
+非同期の更新を行う際に楽観的な評価を行い、APIのレスポンスの前に入力された値を表示する。
+`;
+
+export const useParentPage =
+`### use
+https://ja.react.dev/reference/react/use
+
+非同期の更新を行う際に楽観的な評価を行い、APIのレスポンスの前に入力された値を表示する。
+`;
