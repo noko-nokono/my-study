2fdcc8c929deaf5e0bc7278790c3511f0bbc3def
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-07-09 23:19:28 +0900

    add header responsive

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,9 +1,9 @@
 export const Header = () => {
   return (
     <>
-      <header className="w-[1000px] h-15 flex items-center justify-between px-4 border border-gray-200 mx-auto rounded-full mt-4">
+      <header className="w-[1000px] max-lg:w-auto h-15 flex items-center justify-between px-4 border border-gray-200 mx-auto rounded-full mt-4 max-lg:mx-4">
         <h1 className="text-2xl font-bold">My Study</h1>
-        <img src="/menu.svg" alt="Menu" className="w-6 h-6 cursor-pointer" />
+        <img src="/menu.svg" alt="Menu" className="w-6 h-6 cursor-pointer max-[1001px]:block min-lg:hidden" />
       </header>
     </>
   );
