7995f875fa870f9ec82900951ef5830e07911f24
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-07-09 23:21:08 +0900

    add layout responsive

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -7,7 +7,7 @@ export const Layout = () => {
   return (
     <>
       <Header />
-      <div className='w-[1000px] flex mx-auto mt-4 max-lg:w-full max-lg:mx-4'>
+      <div className='w-[1000px] flex mx-auto mt-4 max-lg:w-auto max-lg:mx-4'>
         <SideMenu />
         <Container>
           <Outlet />
