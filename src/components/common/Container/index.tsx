d3d99437228d3d1336298b5f48b98787a666fd8d
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-07-09 23:40:49 +0900

    add scroll position

--- src/components/common/Container/index.tsx
+++ src/components/common/Container/index.tsx
@@ -6,7 +6,7 @@ export const Container = (props: Props) => {
   const { children } = props;
 
   return (
-    <div className='m-4'>
+    <div className='m-4 overflow-scroll'>
       { children }
     </div>
   )
--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -3,11 +3,13 @@ import { Header } from '@/components/common/Header';
 import { SideMenu } from '@/components/common/SideMenu';
 import { Container } from '@/components/common/Container';
 
+const HEADER_HEIGHT = 92;
+
 export const Layout = () => {
   return (
     <>
       <Header />
-      <div className='w-[1000px] flex mx-auto mt-4 max-lg:w-auto max-lg:mx-4'>
+      <div className={`w-[1000px] flex mx-auto max-lg:w-auto max-lg:mx-4 h-[calc(100vh-${HEADER_HEIGHT}px)]`}>
         <SideMenu />
         <Container>
           <Outlet />
