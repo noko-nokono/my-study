d7f393a80b87c2e7e1c248318185bf8ae4807858
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-07-12 23:03:09 +0900

    add react version 19 description

--- src/page/react-update/version19/page.ts
+++ src/page/react-update/version19/page.ts
@@ -9,34 +9,43 @@ export const useTransitionPage =
 `### useTransition
 https://ja.react.dev/reference/react/useTransition
 
-useTransitionを使用することで、非同期な更新処理に対してのpendingの状態を今までより簡単に管理できるようになる。
+バックグラウンドで UI の一部をレンダリングできる React Hook です。
+本来は、React 18 のバージョンで追加されましたが、React 19 で非同期処理でも使用できるよう拡張が行われました。
+簡単に説明すると、非同期の処理が完了するまでの待機状態（pending）を管理してくれます。
 `;
 
 export const useActionStatePage =
 `### useActionState
 https://ja.react.dev/reference/react/useActionState
 
-useActionStateを使用することで、フォームの値の管理を簡略化してくれる。
-useTransitionを包括する形で説明されていたので、基本はこちらを使うことになりそう？
+フォームアクションの結果に基づいて状態を更新できる React Hook です。
+データの更新を行う際に、その更新結果や状態を使用する場合に活用することができる機能になっています。
+簡単に説明すると、データ更新を行うためのデータ本体や更新処理、待機状態などを管理してくれます。
 `;
 
 export const useFormStatusPage =
 `### useFormStatus
 https://ja.react.dev/reference/react-dom/hooks/useFormStatus
 
-子要素にpropsとしてフォームのデータを渡すのではなく、カスタムフックといて機能を提供してくれている。
+フォーム送信のステータス情報を提供する React Hook です。
+<form>タグを使用したフォームの送信を行う際に、そのフォームの更新状態を子要素でも取得できるようになる機能になっております。
+簡単に説明すると、<form>タグを使用したフォームの送信の待機状態をpropsでバケツリレーせずとも、useFromStateで取得することができます。
 `;
 
 export const useOptimisticPage =
 `### useOptimistic
 https://ja.react.dev/reference/react/useOptimistic
 
-非同期の更新を行う際に楽観的な評価を行い、APIのレスポンスの前に入力された値を表示する。
+UI を楽観的に更新できる React Hook です。
+非同期処理を行う場合に、処理成功後の状態を先にUIに反映を行う機能になっております。
+簡単に説明すると、更新を行う際にサーバーに送信するデータを先にUIに反映し、更新結果を即座にユーザーが見れる状態にすることができます。
 `;
 
 export const useParentPage =
 `### use
 https://ja.react.dev/reference/react/use
 
-非同期の更新を行う際に楽観的な評価を行い、APIのレスポンスの前に入力された値を表示する。
+Promise などのリソースの値を読み取ることができる React API です。
+Suspense の配下で使用でき、async/awaitを使用せずに非同期の処理を行なってくれるような機能になっております。
+簡単に説明すると、Suspenseの配下でデータの取得を行う際、useを使うことで簡単に非同期の実装を行うことができます。
 `;
