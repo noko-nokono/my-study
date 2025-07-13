0d632f94ea14517ce9c8c8c2cbf60bc84ec95c8e
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-07-13 22:43:32 +0900

    add react server component

--- src/page/react-update/server-component/page.ts
+++ src/page/react-update/server-component/page.ts
@@ -4,10 +4,13 @@ export const page =
 
 > This means libraries that ship with Server Components can now target React 19 as a peer dependency with a react-server export condition for use in frameworks that support the Full-stack React Architecture.
 
-React version 19 で追加されたサーバーコンポーネントは、フレームワークで使用されることを前提として機能の追加になっている。
-React Server Component では、Webサーバーを使用しないものと、使用するものの2つの種類が用意されており、一般的にサーバーコンポーネントとして言われているのはWebサーバーを使用する方。
+React version 19 で追加されたサーバーコンポーネントは、フレームワークで使用されることを前提として機能の追加になっています。
 
-使用しないものは、ビルド時に...
+React Server Component では、Webサーバーを使用しないものと、使用するものの2つの種類が用意されており、一般的にサーバーコンポーネントとして言われているのはWebサーバーを使用する方になっています。
+
+Webサーバーを使用しないものは、バックエンドサーバーを持たない環境でも使える仕組みになっており、そのコンポーネントはビルド時に事前に実行され、サーバーコンポーネントと同様の挙動としてクライアントにHTMLを返却します。
+
+Webサーバーを使用するものは、バックエンドサーバーを使用してリクエスト毎にHTMLの生成と返却を行います。
 
 以下公式ドキュメント
 https://ja.react.dev/reference/rsc/server-components
