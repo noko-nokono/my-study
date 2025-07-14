4651b1616cc521fed4b2e6161c3bdbe04fd20653
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-07-14 15:42:22 +0900

    refactor header height

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -3,13 +3,12 @@ import { Header } from '@/components/common/Header';
 import { SideMenu } from '@/components/common/SideMenu';
 import { Container } from '@/components/common/Container';
 
-const HEADER_HEIGHT = 92;
-
 export const Layout = () => {
   return (
     <>
       <Header />
-      <div className={`w-[1000px] flex mx-auto max-lg:w-auto max-lg:mx-4 h-[calc(100vh-${HEADER_HEIGHT}px)]`}>
+      {/* 100vh-92pxの92はHeaderの高さ */}
+      <div className={'w-[1000px] flex mx-auto max-lg:w-auto max-lg:mx-4 h-[calc(100vh-92px)]'}>
         <SideMenu />
         <Container>
           <Outlet />
